161648b57b21a46ef0f200c977151b16f774163f
Anurag Chauhan <68805873+anurag-3523@users.noreply.github.com>
2021-01-08 12:23:17 +0530

    Update Server.py

--- RMI-Calculator/Server.py
+++ RMI-Calculator/Server.py
@@ -1,1 +1,25 @@
+import Pyro4
 
+@Pyro4.expose
+class Calculator(object):
+    
+    def add(self, n1, n2):
+        return n1+n2
+
+    def sub(self, n1, n2):
+        return n1-n2
+    
+    def mult(self, n1, n2):
+        return n1*n2
+    
+    def div(self, n1, n2):
+        return n1/n2 if n2>0 else 0
+
+
+daemon = Pyro4.Daemon.serveSimple({ Calculator: 'Calculator',}, host="localhost", port=8000, ns=False, verbose=True)				
+ns = Pyro4.locateNS() 					
+server = Calculator()					
+uri = daemon.register(server)			
+ns.register('calculator_Server', uri)	
+print('Calculator Server ...')
+daemon.requestLoop()	
